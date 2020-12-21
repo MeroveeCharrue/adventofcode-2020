e00eeb8fb9b44908bd533a0a6f4f5a78fb197180
Mérovée Charrue <merovee@charrue.org>
2020-12-21 21:14:22 +0100

    [day7] --skip--

---
+++ puzzle/day7.php
@@ -0,0 +1,14 @@
+<?php
+
+class day7 extends DayInALife
+{
+    public function getResultPartOne()
+    {
+        // TODO: Implement getResultPartOne() method.
+    }
+
+    public function getResultPartTwo()
+    {
+        // TODO: Implement getResultPartTwo() method.
+    }
+}
