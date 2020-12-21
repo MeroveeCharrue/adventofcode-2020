900d14f3f14df01153050586a5401722c3069126
Mérovée Charrue <merovee@charrue.org>
2020-12-21 21:14:02 +0100

    [day6] try fail part two

--- puzzle/day6.php
+++ puzzle/day6.php
@@ -16,6 +16,16 @@ class day6 extends DayInALife
 
     public function getResultPartTwo()
     {
+        return '';
+
+//        $groups = $this->loadGroups();
+//        $groupSum = 0;
+//
+//        foreach ($groups as $group) {
+//            $groupSum += $this->getGroupCollectiveAnswerSum($group);
+//        }
+//
+//        return $groupSum;
     }
 
     private function getGroupAnswerSum(string $groupAnswers): int
@@ -34,6 +44,36 @@ class day6 extends DayInALife
         }
 
         return count($answerList);
+    }
+
+    private function getGroupCollectiveAnswerSum(string $groupAnswers): int
+    {
+        $answersPerPerson = explode(PHP_EOL, $groupAnswers);
+        $answerList = [];
+
+        foreach ($answersPerPerson as $answers) {
+            $answers = trim($answers);
+
+            $length = strlen($answers);
+            for ($i = 0; $i < $length; $i++) {
+                $answer = $answers[$i];
+
+                if (!array_key_exists($answer, $answerList)) {
+                    $answerList[$answer] = 1;
+                } else {
+                    $answerList[$answer]++;
+                }
+            }
+        }
+
+        $realzy = [];
+        foreach ($answerList as $letter => $count) {
+            if ($count === count($answersPerPerson)) {
+                $realzy[] = $letter;
+            }
+        }
+
+        return count($realzy);
     }
 
     private function loadGroups(): array
